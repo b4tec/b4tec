9f2ff6253706e0cfb8448ae9c03999754ef48bca
MR.DAMAIN <shivagoswami163@gmail.com>
2023-02-25 15:22:37 +0530

    Update Projects.js
    Signed-off-by: MR.DAMAIN <shivagoswami163@gmail.com>

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -37,7 +37,7 @@ function Projects() {
               imgPath={bitsOfCode}
               isBlog={false}
               title="SCRFIRE"
-              description="This program uses the curses library and the random library in Python to create a fire ASCII art. It runs on the command line and displays a graphical representation of fire. It uses random numbers to create shapes that resemble flames, with varying sizes and colors. The user can control the size and color of the fire with the curses library. It can be used in a variety of ways to create different sorts of effects like campfires in a camping game or to add a fiery touch to a text-based game. It can also be used as a decorative piece in an artistic projec"
+              description="This program uses the curses library and the random library in Python to create a fire ASCII art. It runs on the command line and displays a graphical representation of fire. It uses random numbers to create shapes that resemble flames, with varying sizes and colors. The user can control the size and color of the fire with the curses library. It can be used in a variety of ways to create different sorts of effects like campfires in a camping game or to add a fiery touch to a text-based game. It can also be used as a decorative piece in an artistic project."
               ghLink="https://github.com/SHIVADAMAIN/SCRFIRE"
               demoLink="https://youtu.be/ksvd5UXghiE"
             />
