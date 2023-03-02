5bfc01865220595e17f54fa3c22216dbcd7d6c0c
MR.DAMAIN <shivagoswami163@gmail.com>
2023-03-02 11:16:24 +0530

    Update Home2.js
    Signed-off-by: MR.DAMAIN <shivagoswami163@gmail.com>

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -22,7 +22,7 @@ function Home2() {
               I fell in love with programming and I have at least learnt
               something, I think*🤷‍♂️
              <p style={{ color: "rgb(155 126 172)" }}> 
-               " I be decoding my life... "  {" "}
+               " I be decoding my life to escape thy matrix... "  {" "}
              </p>
               <br />I am fluent in classics like
               <i>
